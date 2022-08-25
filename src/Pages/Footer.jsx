e1d8cb7983531ab924b4f20c4f278b21c60e414c
123bhagesh <123bhagesh@gmail.com>
2022-08-25 21:23:00 +0530

    day 3 complete

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import footerStyle from './Footer.module.css' 
+
+const Footer = () => {
+  return (
+    <div className={footerStyle.Container}>
+      <div>
+        
+        <p><a href="">Career Advice</a></p>
+        <p><a href="">Browse Jobs</a></p>
+        <p><a href="">Browse Companies</a></p>
+        <p><a href="">Salaries</a></p>
+        <p><a href="">Indeed Events</a></p>
+        <p><a href="">Work at Indeed</a></p>
+        <p><a href="">Countries</a></p>
+        <p><a href="/about">About</a></p>
+        <p><a href="">Help Center</a></p>
+        <p><a href="">Post a job</a></p>
+        {/* <link rel="stylesheet" href="" /> */}
+
+      </div>
+      <div>
+        <p><a href="">© 2022 Indeed</a></p>
+        <p><a href="">Accessibility at Indeed</a></p>
+        <p><a href="">Privacy Center</a></p>
+        <p><a href="">Cookies</a></p>
+        <p><a href="">Privacy</a></p>
+        <p><a href="">Terms</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -6,16 +6,19 @@ import PostJob from "./PostJob";
 import ReqAuth from "../Components/Signup/pages/ReqAuth";
 import  Logout  from "../Components/Signup/pages/Logout";
 import Register from "../Components/Signup/pages/Register";
+import Footer from "./Footer";
+import About from "./About";
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<ReqAuth><HomePage/></ReqAuth>} />;
+      <Route path="/" element={<HomePage/>} />;
       <Route path="/login" element={<Login />} />
       <Route path="/logout" element={<Logout/>} />
       <Route path="/register" element={<Register />} />
       <Route path="/job/:id" element={''} />
       <Route path="/postjob" element={<ReqAuth> <PostJob /></ReqAuth>}/>
+      <Route path="/about" element={<About/>} />
       <Route path="*" element={<div>Page Not Found</div>} />
       
     </Routes>
